4e96fc4ae1a935d64516287d5da4b9435ff2e43f
Kamil Pro <kprocyszyn@gmail.com>
2023-04-01 10:20:27 +0100

    add quit

--- struct/struct.go
+++ struct/struct.go
@@ -20,25 +20,31 @@ func main() {
 
 	in := bufio.NewReader(os.Stdin)
 
-	fmt.Println("Please select an option")
-	fmt.Println("1) Print Menu")
-	fmt.Println("2) Add item")
-	fmt.Println("q) quit")
-	choice, _ := in.ReadString('\n')
+loop: //this is label, it's what allows break statement to break out of this loop
+	for {
+		fmt.Println("Please select an option")
+		fmt.Println("1) Print Menu")
+		fmt.Println("2) Add item")
+		fmt.Println("q) quit")
+		choice, _ := in.ReadString('\n')
 
-	switch strings.TrimSpace(choice) {
-	case "1":
-		for _, item := range menu {
-			fmt.Println(item.name)
-			fmt.Println(strings.Repeat("-", 10))
-			for size, price := range item.prices {
-				fmt.Printf("\t%10s%10.2f\n", size, price) //%10s is formatting verb
+		switch strings.TrimSpace(choice) {
+		case "1":
+			for _, item := range menu {
+				fmt.Println(item.name)
+				fmt.Println(strings.Repeat("-", 10))
+				for size, price := range item.prices {
+					fmt.Printf("\t%10s%10.2f\n", size, price) //%10s is formatting verb
+				}
 			}
+		case "2":
+			fmt.Println("Please enter a name of new item:")
+			name, _ := in.ReadString('\n')
+			menu = append(menu, menuItem{name: name, prices: make(map[string]float64)})
+		case "q":
+			break loop
+		default:
+			fmt.Println("unknown option")
 		}
-	case "2":
-		fmt.Println("Please enter a name of new item:")
-		name, _ := in.ReadString('\n')
-		menu = append(menu, menuItem{name: name, prices: make(map[string]float64)})
 	}
-
 }
