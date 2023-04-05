cf4710b107fa03018194e4c3c3a981f253500a5f
Kamil Pro <kprocyszyn@gmail.com>
2023-04-05 07:22:52 +0100

    Split code into functions

--- struct/struct.go
+++ struct/struct.go
@@ -7,18 +7,19 @@ import (
 	"strings"
 )
 
+type menuItem struct {
+	name   string
+	prices map[string]float64
+}
+
+var menu = []menuItem{
+	{name: "Coffee", prices: map[string]float64{"small": 1.65, "medium": 1.80, "large": 1.95}},
+	{name: "Espresso", prices: map[string]float64{"single": 1.90, "double": 2.25, "triple": 2.55}},
+}
+
+var in = bufio.NewReader(os.Stdin)
+
 func main() {
-	type menuItem struct {
-		name   string
-		prices map[string]float64
-	}
-
-	menu := []menuItem{
-		{name: "Coffee", prices: map[string]float64{"small": 1.65, "medium": 1.80, "large": 1.95}},
-		{name: "Espresso", prices: map[string]float64{"single": 1.90, "double": 2.25, "triple": 2.55}},
-	}
-
-	in := bufio.NewReader(os.Stdin)
 
 loop: //this is label, it's what allows break statement to break out of this loop
 	for {
@@ -30,21 +31,29 @@ loop: //this is label, it's what allows break statement to break out of this loo
 
 		switch strings.TrimSpace(choice) {
 		case "1":
-			for _, item := range menu {
-				fmt.Println(item.name)
-				fmt.Println(strings.Repeat("-", 10))
-				for size, price := range item.prices {
-					fmt.Printf("\t%10s%10.2f\n", size, price) //%10s is formatting verb
-				}
-			}
+			printMenu()
 		case "2":
-			fmt.Println("Please enter a name of new item:")
-			name, _ := in.ReadString('\n')
-			menu = append(menu, menuItem{name: name, prices: make(map[string]float64)})
+			addItem()
 		case "q":
 			break loop
 		default:
 			fmt.Println("unknown option")
+		}
+	}
+}
+
+func addItem() {
+	fmt.Println("Please enter a name of new item:")
+	name, _ := in.ReadString('\n')
+	menu = append(menu, menuItem{name: name, prices: make(map[string]float64)})
+}
+
+func printMenu() {
+	for _, item := range menu {
+		fmt.Println(item.name)
+		fmt.Println(strings.Repeat("-", 10))
+		for size, price := range item.prices {
+			fmt.Printf("\t%10s%10.2f\n", size, price) //%10s is formatting verb
 		}
 	}
 }
